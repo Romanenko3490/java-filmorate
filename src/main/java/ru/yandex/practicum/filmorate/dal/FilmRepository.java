0c689d62fc21d446b6286779ec80d4f7323062e0
Romanenko3490 <Saff-1@yandex.ru>
2025-07-18 10:12:29 +0300

    Sprint 12 Add class: Poryadocheck

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;

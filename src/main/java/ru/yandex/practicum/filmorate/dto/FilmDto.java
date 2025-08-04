ea44b60004abeda995ea650ef64c6b8ed36c95bb
Romanenko3490 <Saff-1@yandex.ru>
2025-08-04 22:59:39 +0300

    Make central checker/ rework FilmDto
    mpa - Jigi-migi again

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
@@ -6,8 +6,9 @@ import ru.yandex.practicum.filmorate.model.film.Genre;
 import ru.yandex.practicum.filmorate.model.film.MpaRating;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Data
@@ -17,12 +18,12 @@ public class FilmDto {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
-    private List<GenreDto> genres;
+    private Map<Long, String> genres;
     private MpaDto mpa;
     private Set<Director> directors;
 
-
     public FilmDto() {
+        this.genres = new TreeMap<>(); // Инициализация TreeMap для автоматической сортировки
     }
 
     public FilmDto(Long id,
@@ -33,24 +34,30 @@ public class FilmDto {
                    Set<Genre> genres,
                    MpaRating mpa,
                    Set<Director> directors) {
+        this();
         this.id = id;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
-        this.genres = genres.stream()
-                .map(genre -> new GenreDto(genre.getId()))
-                .sorted((g1, g2) -> Long.compare(g1.getId(), g2.getId()))
-                .collect(Collectors.toList());
-        this.mpa = new MpaDto(mpa.getId(), mpa.getName());
+        this.setGenres(genres); // Используем сеттер для установки жанров
+        this.setMpa(mpa);
         this.directors = directors;
     }
 
     public void setGenres(Set<Genre> genres) {
+        if (genres == null) {
+            this.genres = new TreeMap<>();
+            return;
+        }
+
         this.genres = genres.stream()
-                .map(genre -> new GenreDto(genre.getId()))
-                .sorted((g1, g2) -> Long.compare(g1.getId(), g2.getId()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(
+                        Genre::getId,
+                        Genre::getName,
+                        (existing, replacement) -> existing, //функция слияния.
+                        TreeMap::new
+                ));
     }
 
     public void setMpa(MpaRating mpa) {

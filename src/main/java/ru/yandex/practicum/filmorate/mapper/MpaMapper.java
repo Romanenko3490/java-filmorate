29d5a2dfe457cdb07c8c5eced0a8543519eb2f32
Romanenko3490 <Saff-1@yandex.ru>
2025-08-06 20:43:51 +0300

    looks like finish

--- src/main/java/ru/yandex/practicum/filmorate/mapper/MpaMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/MpaMapper.java
@@ -5,9 +5,7 @@ import ru.yandex.practicum.filmorate.model.film.MpaRating;
 
 public class MpaMapper {
     public static MpaDto mapToMpaDto(MpaRating mpa) {
-        MpaDto dto = new MpaDto();
-        dto.setId(mpa.getId());
-        dto.setName(mpa.getName());
+        MpaDto dto = new MpaDto(mpa.getId(), mpa.getName());
         return dto;
     }
 }
